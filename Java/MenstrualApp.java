f79489a06818b06117598794e07c6bb9e0e089a4
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-06 15:18:03 +0100

    Menstrual app

--- Java/MenstrualApp.java
+++ Java/MenstrualApp.java
@@ -7,7 +7,45 @@ public class MenstrualApp {
         public static void main (String[] args) {
                 Scanner keyboardInput = new Scanner(System.in);
                 
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
+                boolean meNU = true;
+                while (meNU) {
+
+                String mainMenu = """
+Welcome to JStarr menstrual calcnic!
+=============================
+What will you like to do today?
+
+Press: 
+1. To calculate the length of your cycle.
+2. To calculate your flow date.
+3. To know your safe period.
+4. To calculate your ovulation date.
+5. To get tips about painful menstruation.
+6. To get tips about irregular menstruation.
+7. To consult a gynecologist.
+8. To exit the app.
+=============================
+                """;
+
+                System.out.println(mainMenu);
+                System.out.print("Press a number to make a selection between 1 - 4: ");
+                int menu = keyboardInput.nextInt();
+
+                if (menu < 0 || menu > 4) {
+                        System.out.println("You entered an invalid option!! Kindly try again or press 0 for Yoruba");
+                        System.out.print("Press a number to make a selection between 1 - 4: ");
+                        menu = keyboardInput.nextInt();
+                        }
+
+                switch (menu) {
+                case 1: { 
+                                EmployeeFunction.addEmployeePayroll();
+                                }
+                
+                
+                
+                
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
                 
                 System.out.print("Enter the date your period started (yyyy-mm-dd): ");
                 String userdate1 = keyboardInput.nextLine();
@@ -17,7 +55,7 @@ public class MenstrualApp {
                 String userdate2 = keyboardInput.nextLine();
                   LocalDate dateTwo = LocalDate.parse(userdate2, formatter);
                 
-
+                  long daysDifference = ChronoUnit.DAYS.between(dateOne, dateTwo);
                 
                   System.out.println(dateTwo);
                 
@@ -26,6 +64,6 @@ public class MenstrualApp {
 
                 System.out.println(dateOne);
               
-              
+               System.out.println(daysDifference);
         }
 }
