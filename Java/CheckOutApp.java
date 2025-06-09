79e4f072c0702c44f8df9184620fd994ce9b96d8
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-09 12:38:06 +0100

    CheckerOut App

---
+++ Java/CheckOutApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CheckOutApp {
+        public static void main (String[] args) {
+        Scanner keyboardInput = new Scanner(System.in);
+    
+        System.out.println("What is the customer's Name: ");
+        String customerName = keyboardInput.next();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
