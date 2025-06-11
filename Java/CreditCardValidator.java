78ff62c7f73cc4e8986f0db5cc94ff9ef1ffe697
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-11 09:25:53 +0100

    creditCardValidator

--- Java/CreditCardValidator.java
+++ Java/CreditCardValidator.java
@@ -5,19 +5,20 @@ public class CreditCardValidator {
 
                 System.out.println("Hello, Kindly Enter Card details to verify: ");
                 String cardDetails = keyboardInput.next();
-              int cardNumber = Integer.parseInt(cardDetails);
-
+              
                 String cardType = CreditCardValidatorFunction.creditCardType(cardDetails);
-                System.out.print(cardType);
+                
+                System.out.println("*************************************");
+                System.out.println("**Credit Card Type: "+ cardType);
+                System.out.println("**Credit Card Number: "+ cardDetails);
 
+                int cardLength = CreditCardValidatorFunction.cardLength(cardDetails);
+                System.out.println("**Credit Card Digit Length: "+ cardLength);
 
+                String cardStatus = cardValidity(cardDetails);
+                System.out.println("**Credit Card Validity Status: "+ cardStatus);
 
-
-
-
-
-
-
+                System.out.println("*************************************");
 
 
         }
--- Java/CreditCardValidatorFunction.java
+++ Java/CreditCardValidatorFunction.java
@@ -1,21 +1,40 @@
+import java.util.Arrays;
 public class CreditCardValidatorFunction {
 
         public static String creditCardType(String cardNum) {
+                String cardName = " ";
+                char firstNum = cardNum.charAt(0);
+                int firstDigit = Character.getNumericValue(firstNum);
 
-
-
-
-                        if (counter == 1 && digits == 4) cardName = "Visa Cards";
-        
-
-                }   
+                char secondNum = cardNum.charAt(1);
+                int secondDigit = Character.getNumericValue(secondNum); 
+                
+                if (cardNum.length() <= 16 && cardNum.length() >= 13) {
+                        if (firstDigit == 4) cardName = "Visa Cards";
+                        else if (firstDigit == 5) cardName = "MasterCard";
+                        else if (firstDigit == 6) cardName = "Discover Cards";
+                        else if (firstDigit == 3 && secondDigit == 7) cardName = "American ExpressCards";
+                        else{
+                                cardName = "Invalid-Card";
+                        }
+                        }
+                else{
+                        cardName = "Invalid Card";
+                        }
+                return cardName; 
         }
 
+        public static int cardLength(String cardNum){
+                int length = cardNum.length();
+                return length;
+        }
+
+        public static String cardValidity(String cardNum){
 
 
 
-
-
+        }
+//int cardNumber = Integer.parseInt(cardDetails);
 
 
 
