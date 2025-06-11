8e63871350876da9de014ba5bf9250174fbe6388
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-11 07:49:46 +0100

    creditCardValidator

--- Java/CreditCardValidator.java
+++ Java/CreditCardValidator.java
@@ -3,10 +3,12 @@ public class CreditCardValidator {
         public static void main(String[] args) {
                 Scanner keyboardInput = new Scanner(System.in);
 
+                System.out.println("Hello, Kindly Enter Card details to verify: ");
+                String cardDetails = keyboardInput.next();
+              int cardNumber = Integer.parseInt(cardDetails);
 
-
-
-
+                String cardType = CreditCardValidatorFunction.creditCardType(cardDetails);
+                System.out.print(cardType);
 
 
 
---
+++ Java/CreditCardValidatorFunction.java
@@ -0,0 +1,27 @@
+public class CreditCardValidatorFunction {
+
+        public static String creditCardType(String cardNum) {
+
+
+
+
+                        if (counter == 1 && digits == 4) cardName = "Visa Cards";
+        
+
+                }   
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
