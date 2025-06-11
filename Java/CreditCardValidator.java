478d823b1f2fd5410ef5a0d1ba23dd6508ddc148
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-11 06:42:09 +0100

    creditCardValidator

---
+++ Java/CreditCardValidator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class CreditCardValidator {
+        public static void main(String[] args) {
+                Scanner keyboardInput = new Scanner(System.in);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+}
