a46e2074dcfee269fa1abc8b7c206a6308f7f9b3
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-11 11:03:36 +0100

    creditCardValidator

--- Java/CreditCardValidatorFunction.java
+++ Java/CreditCardValidatorFunction.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 public class CreditCardValidatorFunction {
 
         public static String creditCardType(String cardNum) {
@@ -30,19 +29,36 @@ public class CreditCardValidatorFunction {
         }
 
         public static String cardValidity(String cardNum){
-                int counter = 2;
-
-        for(int count = 0; count > cardNum.length(); count++){
-                characterss = cardNum.charAt(counter);
-                digits = Character.getNumericValue(characterss);
+                int sum = 0, int add = 0; total = 0;
+              String validity = " ";
+        for(int count = cardNum.length() - 1; count > -1; count-= 2){
+                char characterss = cardNum.charAt(count);
+                int digits = Character.getNumericValue(characterss);
+                int firstResult = digits * 2;
+                int secondResult;
                 
-                counter+2;
-
-
+                if (firstResult >= 10) {
+                        secondResult = (firstResult / 10) + (firstResult % 10);
+                }
+                else{
+                secondResult = firstResult);
+                }
+                sum += secondResult;
         }
-
+        
+        for(int count = 0; count < cardNum.length() -1; count += 2) {
+               char characterss = cardNum.charAt(count);
+                int digits = Character.getNumericValue(characterss);
+                add += digits;
         }
-//int cardNumber = Integer.parseInt(cardDetails);
+        total = sum + add;
+        if (total % 10 == 0) validity = "Valid";
+
+        else{
+        validity = "Invalid";
+        }
+                return validity;
+        }
 
 
 
