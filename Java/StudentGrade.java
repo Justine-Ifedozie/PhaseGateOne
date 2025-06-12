83482dfd03aba7b0c2cfef1f585b6e7f50718c92
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-12 12:48:33 +0100

    Student Grade

--- Java/StudentGrade.java
+++ Java/StudentGrade.java
@@ -4,43 +4,62 @@ public class StudentGrade {
         public static void main(String[] args) {
                 Scanner keyboardInput = new Scanner(System.in);
 
+                int studentsNumber = 0, subjectNumber = 0;
+                
                 ArrayList <Integer> score = new ArrayList <Integer>();
+                //ArrayList <Integer> score = new ArrayList <Integer>();
 
                 System.out.println("How many students do you have? ");
-                int studentsNumber = keyboardInput.nextInt();
+                studentsNumber = keyboardInput.nextInt();
 
                 System.out.println("How many subjects do they offer? ");
-                int subjectNumber = keyboardInput.nextInt();
+                subjectNumber = keyboardInput.nextInt();
 
-                System.out.print("Saving  >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-                System.out.print("Saved successfully");
+                System.out.println("Saving  >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                System.out.println("Saved successfully");
 
 
         for (int count = 1; count != studentsNumber + 1; count++) {
-                System.out.println("Enter score for subject " + counter);
-                int \' = keyboardInput.nextInt();
-
 
 
                 for (int counter = 1; counter != subjectNumber + 1; counter++) {
-                        System.out.print("Entering score for student " + count);
+                        System.out.println("Entering score for student " + count);
                         System.out.println("Enter score for subject " + counter);
-                        int subjectNumber = keyboardInput.nextInt();
+                        int subjectScore = keyboardInput.nextInt();
+                        score.add(subjectScore);
 
-                         System.out.print("Saving  >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-                        System.out.print("Saved successfully");
+                         System.out.println("Saving  >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                         System.out.println("Saved successfully");
                         }
                 }
+int counter = 0;
+
+        System.out.println("=============================================");
+
+        System.out.print("STUDENT \t ");
+        for (int count = 1; count < subjectNumber + 1; count++) {
+                System.out.print("SUB" + count + "\t ");
+        }
+        System.out.println(" TOT \t AVE \t POS ");
+        System.out.println("=============================================");
+        for (int count = 1; count < studentsNumber + 1; count++) {
+                System.out.print("Student " + count);
+                
+int countt = 0;
+                for (; counter < score.size(); counter++) {
+                        System.out.print(" \t " + score.get(counter));
+                        countt++;
+                        if (countt == subjectNumber){
+                         break;
+                        }
+                      }
+                System.out.println();
+        }
 
 
 
-
-
-
-
-
-
-
+        System.out.println("=============================================");
+        System.out.println("=============================================");
 
 
 
