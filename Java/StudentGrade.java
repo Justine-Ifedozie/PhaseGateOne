f7dd776773d73aecab3551b1a7cf89f9c1a6fa2f
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-12 09:52:06 +0100

    Student Grade

--- Java/StudentGrade.java
+++ Java/StudentGrade.java
@@ -1,16 +1,36 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class StudentGrade {
         public static void main(String[] args) {
                 Scanner keyboardInput = new Scanner(System.in);
 
-                System.out.println("Hello, Kindly Enter Card details to verify: ");
-                String cardDetails = keyboardInput.next();
+                ArrayList <Integer> score = new ArrayList <Integer>();
+
+                System.out.println("How many students do you have? ");
+                int studentsNumber = keyboardInput.nextInt();
+
+                System.out.println("How many subjects do they offer? ");
+                int subjectNumber = keyboardInput.nextInt();
+
+                System.out.print("Saving  >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                System.out.print("Saved successfully");
+
+
+        for (int count = 1; count != studentsNumber + 1; count++) {
+                System.out.println("Enter score for subject " + counter);
+                int \' = keyboardInput.nextInt();
 
 
 
+                for (int counter = 1; counter != subjectNumber + 1; counter++) {
+                        System.out.print("Entering score for student " + count);
+                        System.out.println("Enter score for subject " + counter);
+                        int subjectNumber = keyboardInput.nextInt();
 
-
-
+                         System.out.print("Saving  >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+                        System.out.print("Saved successfully");
+                        }
+                }
 
 
 
