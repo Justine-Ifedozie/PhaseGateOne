06aa2df71f5d1f69ad6489a91a8da709d319aeb1
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-13 13:05:07 +0100

    MBTI test

--- Java/PersonalityTest.java
+++ Java/PersonalityTest.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class PersonalityTest {
         public static void main (String[] args) {
                 Scanner keyboardInput = new Scanner(System.in);
-
+                ArrayList<String>selectionDataBase = new ArrayList<>();
+        
         String [] [] arrayQuestions = {{"A. Expend energy, enjoy groups", "B. Conserve energy, enjoy one-on-one"},
                                     {"A. Interpret literally", "B. Looking for meaning and possiblities"},
                                     {"A. Logical, thinking, questioning", "B. Emphatetic, feeling, accomodating"},
@@ -32,37 +33,51 @@ public class PersonalityTest {
         System.out.println(); 
         
         System.out.println("Welcome to MBTI Personality Test!");        
-        System.out.println("You'll be presented with 20 options, kindly select the one that resonates with you more.");
+        System.out.println("You'll be presented with 20 questions categorized into 4 sections, kindly select the one that resonates with you more.");
+        System.out.println("<<<<<<Section One: Extroverted E vs Introverted I>>>>>>");
         System.out.println("Pick A or B for each questions.");
 
         System.out.println();
         
-        for(int count = 0; count < arrayQuestions.length; count++){
+        for(int count = 0; count < arrayQuestions.length; count += 4){
                 System.out.println(arrayQuestions[count][0] + " \t " + arrayQuestions[count][1]);
        
         String userSelection = " ";
         while(true) {
                userSelection = keyboardInput.next().toUpperCase();
        
-               if (userSelection.equals("A") || userSelection.equals("B") {
+               if (userSelection.equals("A") || userSelection.equals("B")) {
+                       selectionDataBase.add(userSelection); 
                        break;
+                 }
               else {
                       System.out.println("Expected A or B as Response \n I know this is an error, Please retry again");
                        }
-       
+                }
        
        
         }
 
-
-
-
-
-
-
-
-
-
-
+        for (int count = 0; count < selectionDataBase.size(); count++){
+                System.out.println(selectionDataBase.get(count));
+        
+        
         }
+    
+        
+        int aCount = 0;
+       int bCount = 0;
+        for (int count = 0; count < selectionDataBase.size(); count++){
+                if (selectionDataBase.get(count).equals("A")){
+                        aCount++;
+                }
+                else{
+                        bCount++;
+        }
+       }
+        System.out.println("Number of A selected: " + aCount);  
+       System.out.println("Number of  B selected: " + bCount);
+       
+       
+     }
 }
--- Java/PersonalityTestFunction.java
+++ Java/PersonalityTestFunction.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class PersonalityTestFunction {
         ArrayList<String>selectionDataBase = new ArrayList<>();
 
-        public                                    
+        public static            
 
 
 
