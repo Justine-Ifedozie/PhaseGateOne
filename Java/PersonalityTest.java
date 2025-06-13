6116efc520a80e49fed37de22e6994051e078421
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-13 08:19:31 +0100

    MBTI test

---
+++ Java/PersonalityTest.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class PersonalityTest {
+        public static void main (String[] args) {
+                Scanner keyboardInput = new Scanner(System.in);
+                ArrayList <String> items = new ArrayList <String>();
+
+                System.out.println("What is your name? ");
+                String name = keyboardInput.nextLine();
+
+
+
+
+
+
+        }
+}
---
+++ Java/PersonalityTestFunction.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class PersonalityTestFunction {
+        String [] Array = {
+                                        {"Expend energy, enjoy groups", "Conserve energy, enjoy one-on-one"},
+                                    {"Interpret literally", "Looking for meaning and possiblities"},
+                                    {"Logical, thinking, questioning", "Emphatetic, feeling, accomodating"},
+                                    {"Orgamized, orderly", "Flexible, adaptable"},
+                                    {"Moreoutgoing, think out loud", "more reserved, think to yourself"},
+                                    {"Practical, realistic, experiential", "Imagimative, innovative, theoretical"},
+                                    {"Candid, straight forward, frank", "Tactful, kind, encouraging"},
+                                    {"Plan, schedule", "Unplanned, spontaneous"},
+                                    {"Seek many tasks, public activities, interaction with others", "Seek private, solidarity activities with quiet to concentrate"},
+                                    {"Standard, usual, conventional", "Different, novel, unique"},
+                                    {"Firm, tend to criticize, hold the line", "Gentle, tend to appreciate, conciliate"},
+                                    {"Regulated, structured", "Easy-going, live and let live"},
+                                    {"External, communicative, express yourself", "Internal, reticent, keep to yourself"},
+                                    {"Focus on here-and-now", "Look to the future, global perspective, big picture"},
+                                    {"Tough-minded, just", "Tender-hearted, merciful"},
+                                    {"Preparation, plan ahead", "Go with the flow, adapt as you go"},
+                                    {"Active, initiate", "Reflective, deliberate"},
+                                    {"Facts, things, what is", "Ideas, dreams, what could be, philosophical"},
+                                    {"Matter of fact, issue-oriented", "Sensitive, people-oriented, compassionate"},
+                                    {"Control, govern", "Latitude, freedom"};
+                                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
