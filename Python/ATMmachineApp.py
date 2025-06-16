f8985f3215f4a38f670020781d3db9acc7a45893
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-16 18:54:56 +0100

    ATM App

--- Python/ATMmachineApp.py
+++ Python/ATMmachineApp.py
@@ -1,22 +1,24 @@
-from ATMmachineAppFunction import create_account, deposit, withdraw, show_balance, view_accounts, accounts
-
+from ATMmachineAppFunction import create_account, close_account, deposit, withdraw, show_balance, view_accounts, accounts, change_pin
 
 
 is_running = True
 while is_running:
         
         welcome_text = """
+=============================
 Welcome to Jstee bank!
-_______________________
-You will be required to
-make a selection:
-1. Create account 
-2. Deposit Funds
-3. Withdraw
-4. Show balance
-5. View accounts in the bank
+=============================
+You will be required to make a selection:
+1. Create account (with first name, last name and pin).
+2. Close account
+3. Deposit money
+4. Withdraw money
+5. Check Account balance
+6. Transfer from one account to another.
+7. Change Pin.
+8. View accounts in the bank
 0. Exit
-_______________________
+=============================
                                 """
         print(welcome_text)
 
@@ -24,33 +26,43 @@ _______________________
 #Driver Class
         import random
 
-        customer_choice = int(input("Make a selection between 1 - 4: "))
+        customer_choice = int(input("Make a selection between 1 - 8: "))
         match customer_choice: 
                 case 1: 
-                        user_name = input("Enter your name: ")
-                        user_phonenumber = input("Enter your phone number: ")
-                        user_account_number = random.randint(1230000000, 50000000000000)
-                        create_account(user_name, user_phonenumber, user_account_number, balance = 0.0)
-                        print("New Account Number: ", user_account_number)
-                        #print(accounts)
+                        first_name = input("Enter first name: ")
+                        last_name = input("Enter last name: ")
+                        user_pin = input("Enter your pin: ")
+                        card_number = random.randint(1230000000, 50000000000000)
+                        create_account(first_name, last_name, user_pin, card_number, balance = 0.0)
+                        print("New ATM card Number: ", card_number)
                 case 2: 
+                        fir_name = input("Enter the first name of the account you wish to close: ")
+                        sec_name = input("Enter the last name of the account you wish to close: ")
+                        close_account(fir_name, sec_name)
+                case 3: 
                         account_name = input("Enter the name of the account you wish to make a deposit into: ")
                         amount = int(input("Enter deposit amount: "))
                         deposit(account_name, amount)
-                        #print(accounts)
-                case 3: 
+                case 4: 
                         account_user = input("Enter the name of the account you wish to withdraw from: ")
                         withdraw_amount = int(input("Enter withdraw amount: "))
                         withdraw(account_user, withdraw_amount)
                         #print(accounts)
-                case 4: 
+                case 5: 
                         account_name_2 = input("Enter the name of the account you wish to view its balance: ")
                         show_balance(account_name_2)
-                case 5: 
+                case 6: 
+                        view_accounts()
+                case 7: 
+                        acc_name = input("Enter the first name of the account you wish to change its pin: ")
+                        old_pin = input("Enter account pin: ")
+                        new_pin = input("Enter a new account pin: ")
+                        change_pin(acc_name, old_pin, new_pin)
+                case 8: 
                         view_accounts()
                 case 0:  
                         is_running = False
                 case _: 
                         print("Invalid input")
 
-print("Thank you! Have a nice day!")
+print("Thank you for trusting Banke Bank! Have a nice day!")
--- Python/ATMmachineAppFunction.py
+++ Python/ATMmachineAppFunction.py
@@ -1,21 +1,31 @@
 accounts = []
 
-def create_account(name, phone, account_number, balance = 0.0):
-        account = [name, phone, account_number, balance]
+def create_account(first_name, last_name, user_pin, card_number, balance = 0.0):
+        account = [first_name, last_name, user_pin, card_number, balance]
         accounts.append(account)
         return accounts
+
+
+def close_account(fir_name, sec_name):
+        for account in accounts:  
+                if account[0] == fir_name and account[1] == sec_name:
+                        del account[0]
+                        print("<<<<<Account closure succesful>>>>>")
+                else:
+                        print("Invalid Account")
+
 
 def deposit(name, amount):
         for account in accounts:
                 if account[0] == name:
-                        account[3] += amount
+                        account[4] += amount
                         return account
         
 def withdraw(name, amount):
         for account in accounts:
                 if account[0] == name:
-                        if account[3] >= amount:
-                                account[3] = account[3] - amount
+                        if account[4] >= amount:
+                                account[4] = account[4] - amount
                                 return account
                         else:
                                 return "Insufficient funds"
@@ -23,7 +33,24 @@ def withdraw(name, amount):
 def show_balance(name):
         for account in accounts:
                 if account[0] == name:
-                        print(f'Your account balance is: ${account[3]}')
+                        print(f'Your account balance is: ${account[4]}')
 
 def view_accounts():
-        print(accounts)               
+        print(accounts)          
+
+
+
+def change_pin(first_name, old_pin, new_pin):
+        for account in accounts:
+                if account[0] == first_name and account[2] == old_pin:
+                        account[2] == new_pin
+                else:
+                        print("Invalid input!")
+
+
+
+
+
+
+
+
