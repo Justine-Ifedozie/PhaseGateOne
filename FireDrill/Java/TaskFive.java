3aeab5eb739b516c7cae9a0fc201e69f6c81abeb
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-06-17 17:23:06 +0100

    Class Task

--- FireDrill/Java/TaskFive.java
+++ FireDrill/Java/TaskFive.java
@@ -2,10 +2,12 @@ public class TaskFive {
 	public static void main (String[] args){
 		int number = 10;
 		for(int counter = 1; counter <= 5; counter ++){
-			for(int count = 4; count <= number; count+=4){
-
-			System.out.println(count);
+			for(int count = 4; count <= number; count+=8){
+			for(int countt = 8; countt <= number; countt+=8){	
+			System.out.print(count);
+			System.out.print(count);
 		}
+	}
 	}
 
 }
